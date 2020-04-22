a08fc76c766a29402c8483b9fe59e8cef17dc7d6
Stephanie Friend <stephaniefriend91@gmail.com>
2020-04-21 22:25:33 -0600

    Remove unneeded variable names

--- app/controllers/merchant/bulk_discounts_controller.rb
+++ app/controllers/merchant/bulk_discounts_controller.rb
@@ -11,7 +11,7 @@ class Merchant::BulkDiscountsController < Merchant::BaseController
 
   def create
     merchant = Merchant.find(params[:merchant_id])
-    bulk_discount = merchant.bulk_discounts.create(bulk_discount_params)
+    merchant.bulk_discounts.create(bulk_discount_params)
 
     redirect_to "/merchant/#{merchant.id}/bulk_discounts"
   end
